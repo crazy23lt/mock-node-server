f18402b93a5e8e4ba50d7ed631cfaf78a072e635
crazy23lt <2394630102@qq.com>
2021-02-19 15:08:28 +0800

    axios请求并发

--- add.js
+++ add.js
@@ -1,11 +1,21 @@
 const axios = require("axios");
 const Mock = require("mockjs");
-const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MTE4MzM1MDgsImV4cCI6MTY0MzM2OTUwOCwiYXVkIjoidXNlciIsInVpZCI6IjYzRkMzOEQyMDY2NEFGNTRDMEZGRTZCNkEwREU3RTREIiwicGhvbmUiOiIxMzYzNjA2NTg5MCJ9.fg5uKGN7mnNSi6Dhp5tch5Up46q_l_buCb260nODZk8PtZ-l3L5qCqffMja--r84CN5OC_qiGmBX8XHveFMT9brnBCDZnpl98cY7Bb6HBwmjWsN-OAy-sl6lyK_4fd5ue22CfrDX0z8CQL2ZfdE2-pIEhicxpXmDfIdekXAHZoQ";
+const Random = Mock.Random;
+const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MTM2MTEyOTgsImV4cCI6MTY0NTE0NzI5OCwiYXVkIjoidXNlciIsInVpZCI6IjYzRkMzOEQyMDY2NEFGNTRDMEZGRTZCNkEwREU3RTREIiwicGhvbmUiOiIxMzYzNjA2NTg5MCJ9.S2yICqtdA6XOhQBxz-vMJbHqE9wfKjhe_AooQB7wMg4FlF0U_a6LlZESvui2Upxn9wrrVyrjmAUDyFw2_aInVlTSHgSfDhh_NBvbUeEZxa0HXTCOytRpYWIJdbusTEJ6luSDpT58foliLmXihxTa2VmZJi068l9L59-HHyZAybQ";
 const instance = axios.create({
     baseURL: "http://223.2.84.230:6201/",
     timeout: 30000,
     headers: { Authorization: "Bearer " + token },
 });
+const timeFn = function () {
+    let time = Random.date();
+    let arrTime = time.split("-");
+    while (arrTime[0] < 2017) {
+        time = Random.date();
+        arrTime = time.split("-");
+    }
+    return time;
+};
 let paramsList = Mock.mock({
     "item|1000": [
         {
@@ -13,6 +23,9 @@ let paramsList = Mock.mock({
             "source_type|1-3": 1,
             "ad_type|1-3": 1,
             "position_type|1-7": 1,
+            "day": function () {
+                return timeFn()
+            }
         },
     ],
 });
@@ -22,4 +35,6 @@ for (let i = 0; i < paramsList.item.length; i++) {
 }
 axios.all(axiosArr).then(axios.spread(function (arg) {
     console.info(arg.data.msg)
-}));
+})).catch(error => {
+    console.error(error)
+});
